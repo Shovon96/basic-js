184640e58f7ea99746b6ceef692f76cd45815b41
Shovon96 <fakhruddinshovon@gmail.com>
2023-09-01 16:22:42 +0600

    echo "# b8a6-build-phtube-Shovon96" >> README.md

---
+++ phero-tube.js
@@ -0,0 +1,100 @@
+const handleCategories = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const data = await response.json();
+    categoriesContainer(data)
+    // console.log(data.data)
+}
+// dynamic add catogories button
+const categoriesContainer = (data) => {
+    const getCategoriesContainer = document.getElementById('categories-container');
+    data.data.forEach((category) => {
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML = `
+        <a onclick="handleCardCategories('${category.category_id}')" class="btn text-black font-bold">${category.category}</a>
+        `
+        // console.log(category.category_id) 
+        getCategoriesContainer.appendChild(createDiv)
+    });
+}
+
+// Handle card
+const handleCardCategories = async (itemsId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${itemsId}`);
+    const data = await response.json();
+    
+    // get card container
+    const getCardContainer = document.getElementById('card-container');
+    // clear cards
+    getCardContainer.textContent = '';
+    // No data/card available
+    if (data.data.length <= 0) {
+        const noCardContainer = document.getElementById('no-card-container')
+        noCardContainer.textContent = ''
+        const noCardCreateDiv = document.createElement('div');
+        noCardCreateDiv.innerHTML = `<img class="ml-[45%]" src="./Icon.png" alt="">
+        <p class="text-4xl font-bold text-black mt-4 text-center">Oops!! Sorry, There is no <br> Content here</p>
+        `
+        noCardContainer.appendChild(noCardCreateDiv);
+    }
+
+    data.data.forEach((item) => {
+        // console.log(item)
+
+        // convert to posted date hour and minute
+        const totalSeconds = `${item.others.posted_date}`;
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+
+        // Create a div and this div innerHTML and appended
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML = `
+            <div class="card bg-base-100 shadow-xl">
+                <figure>
+                    <img class="p-4 rounded-lg h-60 w-80 relative" src="${item?.thumbnail}" alt="">
+                    <p class="bg-black text-white text-sm ml-32 mt-44 px-2 rounded-lg absolute">${item.others?.posted_date ? `${hours} hrs ${minutes} min ago` : ''}</p>
+                    </figure>
+                <div class="card-body">
+                    <div class="flex gap-5">
+                        <img class="w-12 h-12 rounded-full" src="${item?.authors[0]?.profile_picture}" alt="">
+                        <h2 class="text-base font-bold">${item?.title}</h2>
+                    </div>
+                    <div class="ml-20 text-slate-500">
+                        <p class="flex gap-2 my-2">${item?.authors[0]?.profile_name}
+                        <span>${item?.authors[0]?.verified ? '<img src="./verified_icon.svg" alt="">' : ''}</span>
+                        </p>
+                        <p>${item.others.views} views</p>
+                    </div>
+                </div>
+            </div>
+        `
+
+        getCardContainer.appendChild(createDiv)
+    });
+}
+
+// Handle Blog Button
+const getBlogBtn = document.getElementById('btn-blog').addEventListener('click', () => {
+    window.location.href = "blog.html"
+})
+
+
+
+handleCategories()
+handleCardCategories('1000')
+
+
+/*  sort by views
+let rccddata = data.data
+const sortByViewBtn = document.getElementById("sort-by-view-btn");
+sortByViewBtn.addEventListener("click", () => {
+    console.log(rccddata)
+    rccddata = sortArrayByViewsDescending(rccddata);
+});
+
+function sortArrayByViewsDescending(array) {
+    return array.sort((a, b) => {
+        const viewsA = parseInt(a.others.views.replace(/[^\d]/g, ""));
+        const viewsB = parseInt(b.others.views.replace(/[^\d]/g, ""));
+        return viewsB - viewsA;
+    });
+} */
